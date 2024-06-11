49bad870d30f64bbd2c10629fbcc402680cc47cd
salman <salmannisthar123@gmail.com>
2024-06-12 00:21:59 +0530

    first commit

---
+++ src/main/java/com/devstack/lms/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.devstack.lms.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
